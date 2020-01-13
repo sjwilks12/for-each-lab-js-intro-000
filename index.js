13ed055a96b792bc6db413a9f636f498e4ab4779
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2020-01-13 04:11:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array) {
+  array.forEach((element, index, array) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  array.forEach(callback)
+  return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
